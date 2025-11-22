7b3fee7465ada7ba94f99486f3593cbb26c3c635
datyounger <datnhep21@gmail.com>
2025-11-22 18:49:49 +0700

    sửa void

--- include/app/Item.h
+++ include/app/Item.h
@@ -1,35 +1,47 @@
-#ifndef ITEM_H
-#define ITEM_H
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<sstream>
-#include<stdlib.h>
-#include<iomanip>
-#include "Hoadon.h"
-using namespace std;
+#pragma once
+#include <iostream>
+#include <string>
 
-class Item{
-    private:
-        string maItem;
-        string tenItem;
-        int price;
-    public:
-        Item(){}
-        friend std::ostream& operator <<(std::ostream & ,const Item &);
-        friend std::istream& operator >>(std::istream &,Item &);
-        void setmaItem(string maIteam);
-        string getmaItem();
-        void setTenItem(string tenItem);
-        string  getTenItem();
-        void setPrice(int price);
-        int getprice();
-        friend bool check_exist(const Item &x);
-        friend int getInfo(Item*);
-        friend void displayx(Item*p,string x);
-        friend void display(Item*);
-        friend void Add(Item*);
-        friend void Delete(Item*);
-        friend int check_maItem(Item *p,string ma);        
+// Khai báo sớm
+class Item;
+
+/**
+ * @brief Class đại diện cho một Mặt hàng (Item)
+ */
+class Item {
+private:
+    std::string maItem;
+    std::string tenItem;
+    int price;
+
+public:
+    // Constructor (Bạn nên có một constructor)
+    Item() : price(0) {}
+
+    // Destructor
+    ~Item() {}
+
+    // --- GETTERS (CẦN CÓ CONST) ---
+    // Khai báo hàm getmaItem() LÀ const
+    std::string getmaItem() const; 
+
+    // Khai báo hàm getTenItem() LÀ const
+    std::string getTenItem() const; 
+    
+    // Khai báo hàm getprice() LÀ const
+    int getprice() const; 
+
+    // --- SETTERS ---
+    void setmaItem(std::string maIteam);
+    void setTenItem(std::string tenIteam);
+    void setPrice(int price);
+
+    // --- FRIEND FUNCTIONS (Cho phép truy cập private) ---
+    friend std::ostream& operator<<(std::ostream &out, const Item &p);
+    friend std::istream& operator>>(std::istream &in, Item &p);
+    
+    // Nếu bạn không thể truy cập trực tiếp các biến private trong .cpp 
+    // (như ở phiên bản lỗi trước), hãy đảm bảo hai operator này là friend.
+
+    // ... Các hàm thành viên khác (nếu có)
 };
-#endif
